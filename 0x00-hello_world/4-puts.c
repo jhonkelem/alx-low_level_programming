e3e3f7ad08bfc1075e2ada8774cd09a77037533a
root <yohanneskelem@gmail.com>
2022-03-10 13:22:55 -0800

    the 4th task on c programming hello world is done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * Description: main - prints out a string to stdout.
+ *
+ * Return: Always 0 (success)
+ */
 int main (void)
 {
 	puts("programming is like building a multilingual puzzle\n");
