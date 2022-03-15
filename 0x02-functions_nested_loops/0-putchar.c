1025dfdafb55ba6f296757ba51efba47418b6f86
root <yohanneskelem@gmail.com>
2022-03-15 12:51:27 -0700

    the first task on function nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * Description: main - program that prints _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
