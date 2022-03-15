b8031e87bbe3f5e5a662e9230c14ad0b8466c718
root <yohanneskelem@gmail.com>
2022-03-15 16:19:32 -0700

    the zero task on function c is done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include<stdio.h>
-#include<main.h>
+#include "main.h"
 
 /**
  * Description: main - program that prints _putchar
@@ -9,6 +8,23 @@
 
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+_putchar('_');
+
+_putchar('p');
+
+_putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
+
+_putchar('\n');
+
+return (0);
 }
