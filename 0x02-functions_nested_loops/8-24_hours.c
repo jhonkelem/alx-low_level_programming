90724cbb3a2ec38da02fffe3a6f99a34350b9914
root <yohanneskelem@gmail.com>
2022-03-15 17:22:54 -0700

    the 8th task is done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of a day 
+ * Return: Always 0 (success)
+ */
+
+void jack_bauer(void)
+{
+int a;
+int b;
+
+for (a = 0; a <= 23; a++)
+
+{
+for (b = 0; b <= 59; b++)
+
+{
+	_putchar (a / 10 + '0');
+
+	_putchar (a % 10 + '0');
+
+	_putchar (':');
+
+	_putchar (b / 10 + '0');
+
+	_putchar (b % 10 + '0');
+
+	_putchar ('\n');
+
+
+
+	}
+}
+
+}
