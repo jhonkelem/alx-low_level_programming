a026d30d99635610c8081df4005dfd54dc7f5bfc
root <yohanneskelem@gmail.com>
2022-03-16 08:29:33 -0700

    the task zero on debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - test function that print if integer is positive or negative
+ *
+ * Return: Alwyas 0 (success)
+ */
+
+int main(void)
+{
+
+int i;
+i = 0;
+positive_or_negative(i);
+
+return (0);
+
+}
