65b9df26db691883304688f922d7277d94577fd8
root <yohanneskelem@gmail.com>
2022-03-15 17:06:28 -0700

    the six task is done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _abs - value absolute
+ * @n: the int to print
+ * Return: Always 0 (success)
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+
+{
+
+return (n * (-1));
+
+}
+
+
+
+else if (n == 0)
+
+{
+
+	return (0);
+
+}
+
+
+
+else
+
+{
+
+	return (n);
+
+}
+}
