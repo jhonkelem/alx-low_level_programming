a0b7eab419fa351960e6388e0fe51ed8fcf247e6
root <yohanneskelem@gmail.com>
2022-03-17 16:47:31 -0700

    the 8th task on function is done

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,19 +10,41 @@ void print_square(int size)
 
 {
 
-int i, j;
+int i;
+int k;
 
-if (size <= 0)
+if (size > 0)
 
-_putchar('\n');
+{
 
 for (i = 0; i < size; i++)
 
 {
 
-for (j = 0; j < size; j++)
+for (k = 0; k < size; k++)
 
-_putchar('#');
-_putchar('\n');										}
+{
+
+_putchar (35);
+
 }
+
+_putchar ('\n');
+
+
+}
+
+
+}
+
+else
+
+{
+
+_putchar ('\n');
+
+}
+
+
+
 }
