daf952fd7b2d140c8c64250ab7bb4449a0c767fb
root <yohanneskelem@gmail.com>
2022-03-17 15:01:00 -0700

    the 4th task on function is done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_most_numbers(void)
+
+{
+
+
+
+	int i;
+
+
+
+	i = 0;
+
+	while (i < 10)
+
+	{
+
+		if (i != 2 && i != 4)
+
+		{
+
+			_putchar (i + '0');
+
+		}
+
+		i++;
+
+	}
+
+
+
+	_putchar ('\n');
+}
