1134f9adfa79a3e0cce7d1bcebee5314d508b5b1
root <yohanneskelem@gmail.com>
2022-03-17 15:14:17 -0700

    the 6th task on function is done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_line(int n)
+
+{
+
+
+	int i;
+
+
+
+	if (n > 0)
+
+	{
+
+		for (i = 0; i < n; i++)
+
+		{
+
+			_putchar (95);
+
+		}
+
+
+
+		_putchar ('\n');
+
+	}
+
+
+
+	else
+
+	{
+
+		_putchar ('\n');
+
+	}
+
+}
