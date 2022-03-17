66ca6d824155609c183c803456f8e8dae1f9608f
root <yohanneskelem@gmail.com>
2022-03-17 14:52:30 -0700

    the 3rd task on more function is done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+
+{
+
+	int i;
+
+
+
+	i = 0;
+
+	while (i < 10)
+
+	{
+
+		_putchar (i + '0');
+
+		i++;
+
+	}
+
+
+
+	_putchar ('\n')
+}
