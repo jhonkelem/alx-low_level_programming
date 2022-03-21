a5e8fd15b669a294cd0b4df5a4b2bf5ebbae4387
root <yohanneskelem@gmail.com>
2022-03-21 14:01:04 -0700

    the 3rd task on pointers and array is done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints string
+ * @str: pointer to char
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+
+int i = 0;
+while (str[i] != '\0')
+	  
+{
+	    
+_putchar (str[i]);
+	    
+i += 1;
+	  
+}
+_putchar ('\n');
+
+}
