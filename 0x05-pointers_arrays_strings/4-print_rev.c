3f486d6ac0e575046059f8bac8c99f7defe66a18
root <yohanneskelem@gmail.com>
2022-03-21 14:25:31 -0700

    the 4th task on pointer and array is done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ * @s: string to print
+ * Return: Always 0 (success)
+ */
+
+void print_rev(char *s)
+{
+int i;
+
+
+
+i = 0;
+
+while (s[i] != '\0')
+
+{
+
+	i++;
+
+}
+
+
+
+for (i = i - 1 ; i >= 0; i--)
+
+{
+
+	_putchar (s[i]);
+
+}
+
+
+
+_putchar ('\n');
+
+}
