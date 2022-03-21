a38f8a893c6fc988b2ad9b448ae1943526c1c226
root <yohanneskelem@gmail.com>
2022-03-21 14:59:13 -0700

    the 7th task on pointers and array is done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * puts_half - prints every other character of a string
+ * @str: a pointer to an int
+ * Return: not
+ */
+
+void puts_half(char *s)
+{
+int i, last;
+
+
+i = 0;
+
+while (str[i] != '\0')
+
+{
+
+	i++;
+
+}
+
+
+
+last = (i + 1) / 2;
+
+
+
+for (i = last; str[i]; i++)
+
+{
+
+	_putchar (str[i]);
+
+}
+
+
+
+_putchar ('\n');
+
+}
