ebe9d0dbb5a76c8ae6b1d384382f8798aed3203b
root <yohanneskelem@gmail.com>
2022-03-21 17:39:44 -0700

    3rd task done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,16 +9,20 @@
 void _puts(char *str)
 {
 
-int i = 0;
-while (str[i] != '\0')
+	 int l = 0;
+
 	  
-{
-	    
-_putchar (str[i]);
-	    
-i += 1;
-	  
-}
-_putchar ('\n');
+	 while (*(str + l) != '\0')
+		   
+	 {
+			       
+	   _putchar (str[l]);
+			       
+	   l++;
+			     
+	 }
+
+        _putchar ('\n');
 
 }
+
