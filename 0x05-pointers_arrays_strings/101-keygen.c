6658590fb120438eb5692ab3683e02d64616de46
root <yohanneskelem@gmail.com>
2022-03-22 15:01:41 -0700

    updated 11th task is done

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,31 +6,24 @@
 
 #include <time.h>
 
+/**
+ */
 
- int main () 
 
+int main (void)
 {
-	 
-
-	srand ((unsigned int) (time (NULL)));	 
-	int i;
-	char pass[12];
- 
-
-	printf ("Press enter to get a twelve-character password\n");
-	getchar ();
+int num;
 
 
-	for (i = 0; i < 4; i++)
-	pass[i] = rand () % 9;
-       	char capLetter = 'A' + (rand () % 26);
-      	pass[i + 2] = capLetter;
-      	char letter = 'a' + (rand () % 26);
-     	pass[i + 3] = letter;
-       	printf ("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
 
- } 
+srand(time(0));
 
-	printf ("\n\n");	 
+num = rand();
 
-} 
+printf("%i\n", num);
+
+return (0);
+
+}
+
+
