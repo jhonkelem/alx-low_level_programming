a024dfb28fc15be306039166d934544cdeec3b51
root <yohanneskelem@gmail.com>
2022-03-22 16:08:32 -0700

    updated 7th task is done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,41 +7,23 @@
  * Return: void
  */
 
-void puts_half(char *s)
+void puts_half(char *str)
 {
 int i, last;
 
-
-
 i = 0;
-
 while (str[i] != '\0')
-
 {
-
-	i++;
-
+i++;
 }
-
-
 
 last = (i + 1) / 2;
 
-
-
 for (i = last; str[i]; i++)
-
 {
-
-	_putchar (str[i]);
-
+_putchar (str[i]);
 }
 
-
-
 _putchar ('\n');
-
-
-
 }
 
