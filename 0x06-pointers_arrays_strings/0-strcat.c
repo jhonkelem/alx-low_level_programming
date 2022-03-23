061a922518d3cef95b254a9daf5d4988eb1b873c
root <yohanneskelem@gmail.com>
2022-03-23 15:50:42 -0700

    the zero task on more arrays, string and pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,46 +1,43 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
+ *_strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- *
- * Return: void
+ * Return: dest
  */
+
 char *_strcat(char *dest, char *s)
 {
-int i;
-
-	int j;
+int i, j;
 
 
+i = 0;
+while (dest[i] != '\0')
+{
 
-		i = 0;
+	i++;
 
-			while (dest[i] != '\0')
+}
 
-					{
 
-								i++;
 
-									}
+j = 0;
 
-				j = 0;
+while (src[j] != '\0')
 
-					while (src[j] != '\0')
+{
 
-							{
+	dest[i] = src[j];
 
-										dest[i] = src[j];
+	j++;
 
-												i++;
+	i++;
 
-														j++;
+}
 
-															}
+dest[i] = '\0';
 
-						dest[i] = '\0';
-
-							return (dest);
+return (dest);
 
 }
