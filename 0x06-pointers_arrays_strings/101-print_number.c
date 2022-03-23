1bc6f2d79e5de48d323691242403ff60068c8e8d
root <yohanneskelem@gmail.com>
2022-03-23 11:29:14 -0700

    the 9th task on more array, pointers and strings

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+/**
+ *print_number - function that print an integer
+ *n: input value
+ *Return: void
+ */
+
+void print_number(int n)
+{
+int number;
+
+/**
+ *You are not allowed to use long
+ *you are not allowed to use arrays or pointers
+ */ 
+_putchar("enter an integer\n");
+scanf("%d", &number)
+
+_putchar("the integer is: %d\n", number);
+
+return (0);
+}
