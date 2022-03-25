f57cc702adaf78be9823e293886821b2a2a99f2d
root <yohanneskelem@gmail.com>
2022-03-25 13:51:48 -0700

    the 9th task on more array, pointers and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,50 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  *print_number - function that print an integer
- *n: input value
+ *@n: input integer
  *Return: void
  */
 
 void print_number(int n)
+
 {
-int number;
 
-/**
- *You are not allowed to use long
- *you are not allowed to use arrays or pointers
- */ 
+unsigned int m, d, count;
 
-_putchar("enter an integer\n");
-scanf("%d", &number)
-_putchar("the integer is: %d\n", number);
 
-return (0);
+if (n < 0)
+{
+
+	_putchar(45);
+
+	m = n * -1;
+
+}
+else
+{
+
+	m = n;
+
+}
+
+
+d = m;
+count = 1;
+
+
+while (d > 9)
+{
+
+	d /= 10;
+
+	count *= 10;
+}
+
+
+for (; count >= 1; count /= 10)
+{
+
+	_putchar(((m / count) % 10) + 48);
+}
 }
