7e22d943e3bf8a2c8f0df1d4672a98dcd6195984
root <yohanneskelem@gmail.com>
2022-03-28 12:54:51 -0700

    the 6th task on more arrays, strings and pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints chessboard
+ * @: 2d aray of chars
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8]);
+
+{
+
+int i, j;
+
+i = 0;
+
+while (i < 8) /*number repetitions*/
+
+{
+
+	j = 0;
+
+	while (j < 8)
+
+	{
+
+		_putchar (a[i][j]);
+
+		j++;
+
+	}
+
+	_putchar ('\n'); /*print new line*/
+
+	i++; /*add +1*/
+
+}
+
+}
