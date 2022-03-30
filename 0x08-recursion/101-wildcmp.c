6aebdefb72a0dabd782c91486ba9c79ea885944d
root <yohanneskelem@gmail.com>
2022-03-30 13:23:05 -0700

    the 8th task on recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ *wildcmp - compare two strings and  returns 1
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * return: 0
+ */
+
+
+int wildcmp(char *s1, char *s2)
+
+/* compare strings */
+{
+
+if (*s1 == '\0')
+
+{
+
+if (*s2 != '\0' && *s2 == '*')
+
+{
+
+return (wildcmp(s1, s2 + 1));
+
+}
+
+return (*s2 == '\0');
+
+}
+
+if (*s2 == '*')
+
+{
+
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+
+}
+
+else if (*s1 == *s2)
+
+{
+
+return (wildcmp(s1 + 1, s2 + 1));
+
+}
+
+return (0);
+
+}
