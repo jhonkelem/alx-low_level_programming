a6d8cf3770494b9d3e1489c9b22431712b2e883c
root <yohanneskelem@gmail.com>
2022-04-01 16:02:49 -0700

    the task on argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,8 +5,9 @@
 /**
  * num_checker - checks if a given char is number or not
  * @a: char to be checked
- * Return: 1, if its a number, else 0
- */ 
+ * Return: always 0 (success)
+ */
+
 int num_checker(char *a)
 
 {
