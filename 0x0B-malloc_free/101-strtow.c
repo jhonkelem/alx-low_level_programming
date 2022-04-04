276f47119fe21753b9439d1d49f7b5857344caf8
root <yohanneskelem@gmail.com>
2022-04-04 11:01:12 -0700

    the 6th task on malloc, free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,152 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string of words to be split
+ * Return: pointer to an array of strings
+ */
+
+char **strtow(char *str)
+
+{
+
+char **ptr;
+
+int i, k, len, start, end, j = 0;
+
+int words =  countWords(str);
+
+if (!str || !countWords(str))
+return (NULL);
+
+ptr = malloc(sizeof(char *) * (words + 1));
+
+if (!ptr)
+
+return (NULL);
+
+for (i = 0; i < words; i++)
+
+{
+										start = startIndex(str, j);
+										end = endIndex(str, start);
+										len = end - start;
+										ptr[i] = malloc(sizeof(char) * (len + 1));
+										if (!ptr[i])
+
+{
+
+i -= 1;
+
+while (i >= 0)
+
+{
+										free(ptr[i]);
+i--;
+
+}
+						
+free(ptr);
+return (NULL);
+										}
+
+for (k = 0; k < len; k++)
+
+ptr[i][k] = str[start++];
+
+ptr[i][k++] = '\0';
+
+j = end + 1;
+										}
+
+ptr[i] = NULL;
+
+return (ptr);
+
+}
+
+/**
+ * isspace - determines if the characters is a space or not
+ * @c: input char
+ * Return: 1 if true or 0 or not
+ */
+
+int isspace(char c)
+
+{
+
+return (c == ' ');
+
+}
+
+/**
+ * startIndex - returns first index of non-space char
+ * @s: input string
+ * @Index: starting of index
+ * Return: index of first non-space of char
+ */
+
+int startIndex(char *s, int index)
+
+{
+
+while (isspace(*(s + index)))
+index++;
+return (index);
+
+}
+
+/**
+ * endIndex - returns last index of non-space char
+ * @s: input string
+ * @index: starting index
+ * Return: index of last index of non-space char
+ */
+
+int endIndex(char *s, int index)
+
+{
+
+while (!isspace(*(s + index)))
+index++;
+return (index);
+
+}
+
+/**
+ * countwords - count numbers of words in string
+ * @s: input string
+ * Return: numbers of words
+ */
+
+int countwords(char *s)
+
+{
+
+int wordOn = 0;
+int words = 0;
+
+while (*s)
+
+{
+
+if (isSpace(*s) && wordOn)
+
+wordOn = 0;
+
+else if (!isSpace(*s) && !wordOn)
+
+{
+
+wordOn = 1;
+words++;
+										}
+
+s++;
+
+}
+
+return (words);
+
+}
