51bdebbc9925e7308f09579313ded8c3c738215b
root <yohanneskelem@gmail.com>
2022-04-06 13:10:16 -0700

    the task on static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - function that fills memory with constant byte
+ * @s: parameter defined in main, pointer to memory area
+ * @b: parameter defined in main, constant byte
+ * @n: parameter defined in main, number of bytes to be filled
+ * Return: memory adress of function
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+unsigned int i;
+char *tmp = s;
+
+for (i = 0; i < n; i++)
+
+{
+
+*s = b;
+
+s++;
+
+}
+
+s = tmp;
+
+return (s);
+
+}
