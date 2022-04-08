2c0d7f229e12cde01c10f19045f5e93e7be08386
root <yohanneskelem@gmail.com>
2022-04-08 13:58:05 -0700

    the 4th task on more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,41 +4,27 @@
 
 /**
  * _realloc - reallocates memory block using malloc and free
- * @ptr: pointer to the old array
+ * @ptr: pointer
  * @old_size: size of the memory space to allocate in bytes
  * @new_size: size of type
  * Return: void pointer
  */
 
-void *realloc(void *prt, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 {
 
-void *newptr;
+char *clone, *relloc;
 
-if (ptr == null)
+unsigned int i;
 
-{
+if (ptr != NULL)
 
-newptr = malloc(new_size);
+clone = ptr;
 
-if (newptr == NULL)
+else
 
-{
-
-free(ptr);
-										return (NULL);
-										}
-
-free(ptr);
-
-return (newptr);
-
-}
-
-if (old_size == 0)
-
-return (NULL);
+{ return (malloc(new_size)); }
 
 if (new_size == old_size)
 
@@ -46,65 +32,23 @@ return (ptr);
 
 if (new_size == 0 && ptr != NULL)
 
-{
+{ free(ptr);
+
+return (0); }
+
+relloc = malloc(new_size);
+
+if (relloc == NULL)
+
+return (0);
+
+for (i = 0; i < (old_size || i < new_size); i++)
+										{
+										*(relloc + i) = clone[i];
+										}
 
 free(ptr);
 
-return (NULL);
-
-}
-
-if (new_size > old_size)
-
-{
-
-newptr = malloc(new_size);
-
-if (newptr == NULL)
-
-{
-										free(ptr);
-										return (NULL);
-										}
-
-_memcpy(newptr, ptr, old_size);
-										free(ptr);
-										}
-
-return (newptr);
-
-}
-
-#include "main.h"
-#include <stdio.h>
-
-/**
- * _memcpy - function that copies memory area
- * @dest: pointer to memmory area
- * @src: pointer to another memory area
- * @n: number of bytes to be copied from src
- * Return: memory adress of function
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-
-{
-
-unsigned int i;
-char *tmp = dest;
-
-for (i = 0; i < n; i++)
-
-{
-
-*dest = *src;
-
-dest++;
-										src++;
-										}
-
-dest = tmp;
-
-return (dest);
+return (relloc);
 
 }
