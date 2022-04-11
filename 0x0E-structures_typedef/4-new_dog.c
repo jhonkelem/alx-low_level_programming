9fea21bfa60919d331f210940affdca404348473
root <yohanneskelem@gmail.com>
2022-04-11 15:46:00 -0700

    the task on structure, typedf

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,3 +49,59 @@ if (ptr->name == NULL)
 return (NULL);
 
 }
+
+/**
+ * _strdup - function that returns apointer to newly allocated space
+ * @str: string of chars
+ * Return: adresss of the newly allocated space
+ */
+
+char *_strdup(char *str)
+
+{
+
+unsigned int len;
+
+unsigned int i, j;
+
+char *str_copy;
+
+char *tmp = str;
+
+if (str == NULL)
+
+return (NULL);
+
+i = 0;
+
+while (*str++)
+
+i++;
+
+len = i;
+
+str = tmp;
+
+str_copy = malloc(len * sizeof(char) + 1);
+
+if (str_copy == NULL)
+
+return (NULL);
+
+j = 0;
+
+while (j < len)
+
+{
+
+str_copy[j] = str[j];
+
+j++;
+
+}
+
+str_copy[j] = '\0';
+
+return (str_copy);
+
+}	
