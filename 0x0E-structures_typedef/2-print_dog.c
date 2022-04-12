d49956b0dcb57c67b8e44e57b7ee34ccbd6c0efb
root <yohanneskelem@gmail.com>
2022-04-12 15:36:10 -0700

    the tasks on structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,28 +12,26 @@ void print_dog(struct dog *d)
 
 {
 
-if (d == null)
+if (d == 0)
 
 return;
 
-{
-
-if (d->name != NULL)
-
-printf("Name: %s\n", d->name);
-
-else
+if (d->name == 0)
 
 printf("Name: (nil)\n");
 
+else
+
+printf("Name: %s\n", d->name);
+
 printf("Age: %f\n", d->age);
 
-if (d->owner != NULL)
+if (d->owner == 0)
 
-printf("Owner: %s\n", d->owner);
+printf("Owner: (nil)\n");
 
 else
 
-printf("Owner: (nil)\n");
+printf("Owner: %s\n", d->owner);
 
 }
