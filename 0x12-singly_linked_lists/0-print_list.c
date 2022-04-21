959fd30341564ca0b53b46c4506dcee3dd119000
root <yohanneskelem@gmail.com>
2022-04-21 07:01:41 -0700

    the tasks on singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,24 +7,38 @@
  *
  * Return: The number of nodes in the list
  */
-size_t print_list(const list_t *h)
-{
-	size_t num = 0;
-	list_t *p = h ? h->next : NULL;
 
-	if (h)
-	{
-		printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
-		num++;
-	}
-	if (p)
-	{
-		while (p)
-		{
-			printf("[%d] %s\n", p->str ? p->len : 0, p->str ? p->str : "(nil)");
-			num++;
-			p = p->next;
-		}
-	}
-	return (num);
+size_t print_list(const list_t *h)
+
+{
+
+size_t num = 0;
+list_t *p = h ? h->next : NULL;
+
+if (h)
+{
+printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
+num++;
+
+}
+
+if (p)
+
+{
+
+while (p)
+
+{
+
+printf("[%d] %s\n", p->str ? p->len : 0, p->str ? p->str : "(nil)");
+
+num++;
+p = p->next;
+
+}
+
+}
+
+return (num);
+
 }
