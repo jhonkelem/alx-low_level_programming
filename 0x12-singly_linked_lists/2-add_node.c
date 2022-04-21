e89a51fe20888fdfcc816cc2ecda01cf4289d418
jhonkelem <99354658+jhonkelem@users.noreply.github.com>
2022-04-21 20:49:13 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,47 +14,31 @@ list_t *add_node(list_t **head, const char *str)
 {
 
 unsigned int len = 0, size;
+	char *str1;
+	list_t *new_node;
 
-char *str1;
-
-list_t *new_node;
-
-for (len = 0; str && str[len] != '\0'; len++)
-
-;
-
-if (head)
-
-{
-
-size = sizeof(list_t);
-										new_node = malloc(size);
-
-if (new_node)
-
-{
-
-str1 = malloc(sizeof(char) * (len + 1));
-if (!str1)
-
-{
-	free(new_node);
-	return (NULL);
-}
-
-for (len = 0; str[len] != '\0'; len++)
-
-		str1[len] = str[len];
-		
-	str1[len] = '\0';
-											new_node->str = str1;
-											new_node->len = len;
-											new_node->next = *head;
-	*head = new_node;						
-}
-
-}
-
-return (new_node);
-
+	for (len = 0; str && str[len] != '\0'; len++)
+		;
+	if (head)
+	{
+		size = sizeof(list_t);
+		new_node = malloc(size);
+		if (new_node)
+		{
+			str1 = malloc(sizeof(char) * (len + 1));
+			if (!str1)
+			{
+				free(new_node);
+				return (NULL);
+			}
+			for (len = 0; str[len] != '\0'; len++)
+				str1[len] = str[len];
+			str1[len] = '\0';
+			new_node->str = str1;
+			new_node->len = len;
+			new_node->next = *head;
+			*head = new_node;
+		}
+	}
+	return (new_node);
 }
