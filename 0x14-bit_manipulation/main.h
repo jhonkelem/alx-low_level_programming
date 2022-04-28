8c1248e8e4443fabccad7dc0c8fee71db016c746
root <yohanneskelem@gmail.com>
2022-04-28 12:36:51 -0700

    the tasks on bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,12 +4,17 @@
 #include <stddef.h>
 
 int _putchar(char c);
+int _strlen(const char *s);
+
+int _pow_recursion(int x, int y);
 
 unsigned int binary_to_uint(const char *b);
 
 void print_binary(unsigned long int n);
 
 int get_bit(unsigned long int n, unsigned int index);
+
+void recursion_print_binary(unsigned long int n);
 
 int set_bit(unsigned long int *n, unsigned int index);
 
