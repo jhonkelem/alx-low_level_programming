36940cc7f492419fefc1e3ab245b05a58ee75624
root <yohanneskelem@gmail.com>
2022-05-02 12:26:59 -0700

    the tasks on the file I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,31 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+lose_fd(int fd);
 
-#endif /* MAIN_H */
+void copy_contents(int from_fd, int to_fd, char *src_file, char *dest_file);
+
+void close_fd(int fd);
+
+char is_elf_file(int fd, void **header);
+
+void print_elf_header(void *header);
+
+void print_section(int id, void *header);
+
+void print_magic(void *header);
+
+void print_class(void *header);
+
+void print_data(void *header);
+
+void print_version(void *header);
+
+void print_os_abi(void *header);
+
+void print_abi_version(void *header);
+
+void print_type(void *header);
+
+void print_entry_pt_addr(void *header);
+#endif
