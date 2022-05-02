e8acb8dec5b281f617487d781f3fac2610b0cb3d
jhonkelem <99354658+jhonkelem@users.noreply.github.com>
2022-05-02 21:13:50 +0200

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,38 +6,24 @@
  * @text_content: the NULL terminated string to add at end of file
  * Return: 1 on success, -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
-
-int fd, i;
-
-if (filename == NULL)
-
-	return (-1);
-
-file = open(filename, O_APPEND | O_RDWR, 0664);
-
-if (file == -1)
-return (-1);
-
-if (text_content != NULL)
-
 {
+    int file, fwrite, i;
 
-for (i = 0; text_content[i]; i++)
+    if (filename == NULL)
+        return (-1);
+    file = open(filename, O_APPEND | O_RDWR, 0664);
+    if (file == -1)
+        return (-1);
 
-;
-
-fwrite = write(file, text_content, i);
-
-if (fwrite == -1)
-
-return (-1);
-
-}
-
-close(file);
-
-return (1);
-
+    if (text_content != NULL)
+    {
+        for (i = 0; text_content[i]; i++)
+            ;
+        fwrite = write(file, text_content, i);
+        if (fwrite == -1)
+            return (-1);
+    }
+    close(file);
+    return (1);
 }
